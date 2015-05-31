d7fe446f5aafffa320d58444891f342b42de5c25
Anders Evenrud <andersevenrud@gmail.com>
2015-05-31 18:18:38 +0200

    Implemented IFrameApplication for simplification

--- Calendar/main.js
+++ Calendar/main.js
@@ -1,84 +1,22 @@
-(function(Application, Window, GUI, Dialogs, Utils, API, VFS) {
+(function(Application, GUI, Dialogs, Utils, API, VFS) {
 
-  /////////////////////////////////////////////////////////////////////////////
-  // WINDOWS
-  /////////////////////////////////////////////////////////////////////////////
-
-  /**
-   * Main Window Constructor
-   */
-  var ApplicationCalendarWindow = function(app, metadata) {
-    Window.apply(this, ['ApplicationCalendarWindow', {
-      icon: metadata.icon,
+  var ApplicationCalendar = function(args, metadata) {
+    Application.apply(this, ['ApplicationCalendar', args, metadata, {
+      src: 'data/index.html',
       title: metadata.name,
+      icon: metadata.icon,
       width: 230,
       height: 236,
       allow_resize: false,
       allow_restore: false,
       allow_maximize: false
-    }, app]);
-
-    this.iframeWindow = null;
-  };
-
-  ApplicationCalendarWindow.prototype = Object.create(Window.prototype);
-
-  ApplicationCalendarWindow.prototype.init = function(wmRef, app) {
-    var self = this;
-    var root = Window.prototype.init.apply(this, arguments);
-    var src = API.getApplicationResource(app, 'data/index.html');
-    this._addGUIElement(new GUI.IFrame('CalendarIframe', {src: src}), root);
-    return root;
-  };
-
-  ApplicationCalendarWindow.prototype._inited = function() {
-    Window.prototype._inited.apply(this, arguments);
-  };
-
-  ApplicationCalendarWindow.prototype.destroy = function() {
-    Window.prototype.destroy.apply(this, arguments);
-  };
-
-  /////////////////////////////////////////////////////////////////////////////
-  // APPLICATION
-  /////////////////////////////////////////////////////////////////////////////
-
-  /**
-   * Application constructor
-   */
-  var ApplicationCalendar = function(args, metadata) {
-    Application.apply(this, ['ApplicationCalendar', args, metadata]);
+    }]);
   };
 
   ApplicationCalendar.prototype = Object.create(Application.prototype);
 
-  ApplicationCalendar.prototype.destroy = function() {
-    return Application.prototype.destroy.apply(this, arguments);
-  };
-
-  ApplicationCalendar.prototype.init = function(settings, metadata) {
-    var self = this;
-
-    Application.prototype.init.apply(this, arguments);
-
-    // Create your main window
-    var mainWindow = this._addWindow(new ApplicationCalendarWindow(this, metadata));
-  };
-
-  ApplicationCalendar.prototype._onMessage = function(obj, msg, args) {
-    Application.prototype._onMessage.apply(this, arguments);
-
-    // Make sure we kill our application if main window was closed
-    if ( msg == 'destroyWindow' && obj._name === 'ApplicationCalendarWindow' ) {
-      this.destroy();
-    }
-  };
-
-  //
-  // EXPORTS
-  //
   OSjs.Applications = OSjs.Applications || {};
   OSjs.Applications.ApplicationCalendar = OSjs.Applications.ApplicationCalendar || {};
   OSjs.Applications.ApplicationCalendar.Class = ApplicationCalendar;
 
-})(OSjs.Core.Application, OSjs.Core.Window, OSjs.GUI, OSjs.Dialogs, OSjs.Utils, OSjs.API, OSjs.VFS);
+})(OSjs.Helpers.IFrameApplication, OSjs.GUI, OSjs.Dialogs, OSjs.Utils, OSjs.API, OSjs.VFS);
--- GoogleMaps/main.js
+++ GoogleMaps/main.js
@@ -1,79 +1,22 @@
-(function(Application, Window, GUI, Dialogs, Utils, API, VFS) {
+(function(Application, GUI, Dialogs, Utils, API, VFS) {
 
-  /////////////////////////////////////////////////////////////////////////////
-  // WINDOWS
-  /////////////////////////////////////////////////////////////////////////////
-
-  /**
-   * Main Window Constructor
-   */
-  var ApplicationMapWindow = function(app, metadata) {
-    Window.apply(this, ['ApplicationMapWindow', {
-      icon: metadata.icon,
+  var ApplicationMap = function(args, metadata) {
+    Application.apply(this, ['ApplicationMap', args, metadata, {
+      src: 'data/index.html',
       title: metadata.name,
+      icon: metadata.icon,
       width: 640,
       height: 400,
-      allow_resise: false,
+      allow_resize: false,
       allow_restore: false,
       allow_maximize: false
-    }, app]);
-
-    this.iframeWindow = null;
-  };
-
-  ApplicationMapWindow.prototype = Object.create(Window.prototype);
-
-  ApplicationMapWindow.prototype.init = function(wmRef, app) {
-    var self = this;
-    var root = Window.prototype.init.apply(this, arguments);
-    var src = API.getApplicationResource(app, 'data/index.html');
-
-    this._addGUIElement(new GUI.IFrame('MapIframe', {src: src}), root);
-    return root;
-  };
-
-  ApplicationMapWindow.prototype._inited = function() {
-    Window.prototype._inited.apply(this, arguments);
-  };
-
-  ApplicationMapWindow.prototype.destroy = function() {
-    Window.prototype.destroy.apply(this, arguments);
-  };
-
-  /////////////////////////////////////////////////////////////////////////////
-  // APPLICATION
-  /////////////////////////////////////////////////////////////////////////////
-
-  /**
-   * Application constructor
-   */
-  var ApplicationMap = function(args, metadata) {
-    Application.apply(this, ['ApplicationMap', args, metadata]);
+    }]);
   };
 
   ApplicationMap.prototype = Object.create(Application.prototype);
 
-  ApplicationMap.prototype.destroy = function() {
-    return Application.prototype.destroy.apply(this, arguments);
-  };
-
-  ApplicationMap.prototype.init = function(settings, metadata) {
-    Application.prototype.init.apply(this, arguments);
-    var mainWindow = this._addWindow(new ApplicationMapWindow(this, metadata));
-  };
-
-  ApplicationMap.prototype._onMessage = function(obj, msg, args) {
-    Application.prototype._onMessage.apply(this, arguments);
-    if ( msg == 'destroyWindow' && obj._name === 'ApplicationMapWindow' ) {
-      this.destroy();
-    }
-  };
-
-  //
-  // EXPORTS
-  //
   OSjs.Applications = OSjs.Applications || {};
   OSjs.Applications.ApplicationMap = OSjs.Applications.ApplicationMap || {};
   OSjs.Applications.ApplicationMap.Class = ApplicationMap;
 
-})(OSjs.Core.Application, OSjs.Core.Window, OSjs.GUI, OSjs.Dialogs, OSjs.Utils, OSjs.API, OSjs.VFS);
+})(OSjs.Helpers.IFrameApplication, OSjs.GUI, OSjs.Dialogs, OSjs.Utils, OSjs.API, OSjs.VFS);
